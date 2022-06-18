e810500599bdd2a027138322cad281e12c9ddf7b
Austin Johnson <austintjohnson92@gmail.com>
2022-06-17 20:56:31 -0700

    wip

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,18 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig');
+
+function find() {
+   return db('tasks')
+   .then((tasks) => 
+   tasks.map((task) => ({
+    ...task,
+   task_completed: task.task_completed ? true : false,
+   })) 
+   )
+   .catch((err) => console.log(err.message))
+}
+
+module.exports = {
+    find,
+    
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,14 @@
 // build your `/api/tasks` router here
+const express = require('express');
+const helpers = require('./model');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    helpers.find()
+    .then(tasks => {
+        res.status(200).json(tasks)
+    })
+    .catch(next)
+})
+
+module.exports = router;
