ace52b473f3b0a3066dc32dab69dfa7ff16cf7c6
Austin Johnson <austintjohnson92@gmail.com>
2022-06-17 22:11:16 -0700

    wip

--- api/task/model.js
+++ api/task/model.js
@@ -2,28 +2,47 @@
 const db = require('../../data/dbConfig');
 
 function find() {
-   return db('tasks as t')
-   .join('projects as p', 't.project_id', 'p.project_id')
-   .select(
-    't.task_description',
-    't.task_notes',
-    't.task_completed',
-    'p.project_name',
-    'p.project_description'
-   )
-   .then((tasks) => 
-   tasks.map((task) => ({
-    ...task,
-   task_completed: task.task_completed ? true : false,
-   })) 
-   )
-   .catch((err) => console.log(err.message))
+    return db('tasks as t')
+        .join('projects as p', 't.project_id', 'p.project_id')
+        .select(
+            't.task_description',
+            't.task_notes',
+            't.task_completed',
+            'p.project_name',
+            'p.project_description'
+        )
+        .then((tasks) =>
+            tasks.map((task) => ({
+                ...task,
+                task_completed: task.task_completed ? true : false,
+            }))
+        )
+        .catch((err) => console.log(err.message))
 }
 
-
+function insert(task) {
+    return db('tasks').insert(task)
+        .then(() => {
+            return db('tasks as t')
+                .join(
+                    't.task_description',
+                    't.task_notes',
+                    't.task_completed',
+                    'p.project_name',
+                    'p.project_description'
+                )
+        })
+        .then((tasks) =>
+        tasks.map((task) => ({
+            ...task,
+            task_completed: task.task_completed ? true : false,
+        }))
+    )
+    .catch((err) => console.log(err.message))
+}
 
 module.exports = {
     find,
-   
-    
+    insert
+
 }
